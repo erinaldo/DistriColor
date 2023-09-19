5ac810903906463004a041b28ca949c4a90a1779
agustinpaolucci <111289424+agustinpaolucci@users.noreply.github.com>
2023-09-19 16:15:46 -0300

    Cbo Formas de Pago y  Cbo Articulos. CARGADOS. Campos inicializados.

--- Presentacion/FrmNuevaFactura.cs
+++ Presentacion/FrmNuevaFactura.cs
@@ -21,6 +21,20 @@ namespace DistriColor.Presentacion
         private void FrmNuevaFactura_Load(object sender, EventArgs e)
         {
             ProximaFactura();
+            CargarFormasPago();
+            CargarArticulos();
+            Inicializar();
+
+        }
+
+        private void Inicializar()
+        {
+            txtFecha.Text = DateTime.Today.ToString();
+            txtCliente.Text = "CONSUMIDOR FINAL";
+            txtCantidad.Text = "1";
+            txtSubTotal.Text = "0";
+            txtTotal.Text = "0";
+            txtCliente.Focus();
         }
 
         private void ProximaFactura()
@@ -38,5 +52,41 @@ namespace DistriColor.Presentacion
             conexion.Close();
             lblNroFactura.Text = lblNroFactura.Text + parametro.Value;
         }
+
+        private void CargarFormasPago()
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=ALITTLESHOP_DB;Integrated Security=True");
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = "SP_CONSULTAR_FORMAS_PAGO";
+            conexion.Open();
+            DataTable tabla = new DataTable();
+            tabla.Load(comando.ExecuteReader());
+            cboFormasPago.DataSource = tabla;
+            cboFormasPago.ValueMember = "codigo_formapago";
+            cboFormasPago.DisplayMember = "nombre_formapago";
+            cboFormasPago.DropDownStyle = ComboBoxStyle.DropDownList;
+            conexion.Close();
+        }
+
+        private void CargarArticulos()
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=ALITTLESHOP_DB;Integrated Security=True");
+            SqlCommand comando = new SqlCommand();
+            conexion.Open();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.CommandText = "SP_CONSULTAR_ARTICULOS";
+            DataTable tabla = new DataTable();
+            tabla.Load(comando.ExecuteReader());
+            conexion.Close();
+            cboArticulos.DataSource = tabla;
+            cboArticulos.ValueMember = "codigo_art"; // ojo los nombres, CHEQUEAR
+            cboArticulos.DisplayMember = "nombre_art"; // ojo los nombres, CHEQUEAR
+            cboArticulos.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboArticulos.SelectedIndex = -1;
+        }
+
     }
 }
