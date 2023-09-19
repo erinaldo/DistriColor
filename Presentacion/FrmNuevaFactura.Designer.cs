a474baeeb029e8723d26766557e3ea28da8a1e0f
agustinpaolucci <111289424+agustinpaolucci@users.noreply.github.com>
2023-09-19 15:55:52 -0300

    Capas, clases y formulario Nueva Factura CREADAS.

--- Presentacion/FrmNuevaFactura.Designer.cs
+++ Presentacion/FrmNuevaFactura.Designer.cs
@@ -28,21 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblNroFactura = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.lblFormasPago = new System.Windows.Forms.Label();
+            this.lblSubTotal = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.cboFormasPago = new System.Windows.Forms.ComboBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.brnCancelar = new System.Windows.Forms.Button();
+            this.cboArticulos = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtSubTotal = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblNroFactura
+            // 
+            this.lblNroFactura.AutoSize = true;
+            this.lblNroFactura.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNroFactura.Location = new System.Drawing.Point(480, 9);
+            this.lblNroFactura.Name = "lblNroFactura";
+            this.lblNroFactura.Size = new System.Drawing.Size(264, 43);
+            this.lblNroFactura.TabIndex = 0;
+            this.lblNroFactura.Text = "FACTURA N°:";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(78, 45);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(58, 17);
+            this.lblFecha.TabIndex = 1;
+            this.lblFecha.Text = "Fecha:";
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Location = new System.Drawing.Point(72, 90);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(64, 17);
+            this.lblCliente.TabIndex = 2;
+            this.lblCliente.Text = "Cliente:";
+            // 
+            // lblFormasPago
+            // 
+            this.lblFormasPago.AutoSize = true;
+            this.lblFormasPago.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFormasPago.Location = new System.Drawing.Point(13, 133);
+            this.lblFormasPago.Name = "lblFormasPago";
+            this.lblFormasPago.Size = new System.Drawing.Size(123, 17);
+            this.lblFormasPago.TabIndex = 3;
+            this.lblFormasPago.Text = "Forma de Pago:";
+            // 
+            // lblSubTotal
+            // 
+            this.lblSubTotal.AutoSize = true;
+            this.lblSubTotal.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubTotal.Location = new System.Drawing.Point(559, 370);
+            this.lblSubTotal.Name = "lblSubTotal";
+            this.lblSubTotal.Size = new System.Drawing.Size(123, 24);
+            this.lblSubTotal.TabIndex = 4;
+            this.lblSubTotal.Text = "Sub Total $";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(592, 403);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(90, 28);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "Total $";
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(142, 42);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(153, 20);
+            this.txtFecha.TabIndex = 6;
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(142, 87);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(323, 20);
+            this.txtCliente.TabIndex = 7;
+            // 
+            // cboFormasPago
+            // 
+            this.cboFormasPago.FormattingEnabled = true;
+            this.cboFormasPago.Location = new System.Drawing.Point(142, 133);
+            this.cboFormasPago.Name = "cboFormasPago";
+            this.cboFormasPago.Size = new System.Drawing.Size(323, 21);
+            this.cboFormasPago.TabIndex = 8;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(103, 396);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(182, 34);
+            this.btnAceptar.TabIndex = 9;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // brnCancelar
+            // 
+            this.brnCancelar.Location = new System.Drawing.Point(315, 397);
+            this.brnCancelar.Name = "brnCancelar";
+            this.brnCancelar.Size = new System.Drawing.Size(182, 34);
+            this.brnCancelar.TabIndex = 10;
+            this.brnCancelar.Text = "Cancelar";
+            this.brnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // cboArticulos
+            // 
+            this.cboArticulos.FormattingEnabled = true;
+            this.cboArticulos.Location = new System.Drawing.Point(15, 178);
+            this.cboArticulos.Name = "cboArticulos";
+            this.cboArticulos.Size = new System.Drawing.Size(446, 21);
+            this.cboArticulos.TabIndex = 11;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(479, 178);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(147, 20);
+            this.txtCantidad.TabIndex = 12;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(644, 175);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(144, 23);
+            this.btnAgregar.TabIndex = 13;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 205);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 150);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // txtSubTotal
+            // 
+            this.txtSubTotal.Location = new System.Drawing.Point(688, 370);
+            this.txtSubTotal.Name = "txtSubTotal";
+            this.txtSubTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtSubTotal.TabIndex = 15;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(688, 411);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 16;
             // 
             // FrmNuevaFactura
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 453);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtSubTotal);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.cboArticulos);
+            this.Controls.Add(this.brnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.cboFormasPago);
+            this.Controls.Add(this.txtCliente);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblSubTotal);
+            this.Controls.Add(this.lblFormasPago);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblNroFactura);
             this.Name = "FrmNuevaFactura";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "FrmNuevaFactura";
             this.Load += new System.EventHandler(this.FrmNuevaFactura_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblNroFactura;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.Label lblFormasPago;
+        private System.Windows.Forms.Label lblSubTotal;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.ComboBox cboFormasPago;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button brnCancelar;
+        private System.Windows.Forms.ComboBox cboArticulos;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtSubTotal;
+        private System.Windows.Forms.TextBox txtTotal;
     }
 }
--- Presentacion/FrmNuevaFactura.cs
+++ Presentacion/FrmNuevaFactura.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,8 @@ namespace DistriColor.Presentacion
 
         private void FrmNuevaFactura_Load(object sender, EventArgs e)
         {
-
+            
         }
+
     }
 }
